bbc50e976557629e35f8e1640d5f990c48c3e9a1
stanley2058 <stanley2058@yahoo.com.tw>
2022-01-16 14:28:09 +0800

    feat: add Fixed Ratio normalization

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -62,18 +62,38 @@ export default class Utils {
     return strategy(input);
   }
   static readonly NormalizeStrategy = {
+    /**
+     * Format to [0.1 ~ 1]
+     * @param input
+     * @returns Array of number between 0.1 and 1
+     */
     BetweenFixedNumber(input: number[]) {
-      // format to [0.1 ~ 1]
       const baseLine = 0.1;
       const ratio = 1 - baseLine;
       const max = Math.max(...input);
       const min = Math.min(...input);
+      if (max - min === 0) return input;
       return input.map(
         (value) => ((value - min) / (max - min)) * ratio + baseLine
       );
     },
+    /**
+     * Sigmoid function
+     * @param input
+     * @returns Array of number between 0 and 1
+     */
     Sigmoid(input: number[]) {
       return input.map((value) => 1 / (1 + Math.exp(-value)));
+    },
+    /**
+     * Divide by max value
+     * @param input
+     * @returns Array of number between 0 and 1
+     */
+    FixedRatio(input: number[]) {
+      const max = Math.max(...input);
+      if (max === 0) return input;
+      return input.map((value) => value / max);
     },
   };
 }
