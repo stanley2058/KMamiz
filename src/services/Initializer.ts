e70565d413208395e18c5371cd7b188028366902
stanley2058 <stanley2058@yahoo.com.tw>
2022-01-29 15:43:35 +0800

    feat: implements first time setup

--- src/services/Initializer.ts
+++ src/services/Initializer.ts
@@ -1,6 +1,11 @@
+import { Trace } from "../classes/Trace";
+import IReplicaCount from "../entities/IReplicaCount";
 import GlobalSettings from "../GlobalSettings";
+import KubernetesService from "./KubernetesService";
+import MongoOperator from "./MongoOperator";
 import Scheduler from "./Scheduler";
 import ServiceOperator from "./ServiceOperator";
+import ZipkinService from "./ZipkinService";
 
 export default class Initializer {
   private static instance?: Initializer;
@@ -8,7 +13,28 @@ export default class Initializer {
 
   private constructor() {}
 
-  firstTimeSetup() {}
+  async firstTimeSetup() {
+    const traces = new Trace(
+      await ZipkinService.getInstance().getTraceListFromZipkinByServiceName(
+        86400000 * 30
+      )
+    );
+    const realtimeData = traces.toRealTimeData();
+    const replicas: IReplicaCount[] = [];
+    for (const ns of realtimeData.realtimeData.reduce(
+      (prev, curr) => prev.add(curr.namespace),
+      new Set<string>()
+    )) {
+      replicas.push(
+        ...(await KubernetesService.getInstance().getReplicasFromPodList(ns))
+      );
+    }
+
+    const { aggregateData, historyData } =
+      traces.toAggregatedDataAndHistoryData(replicas);
+    await MongoOperator.getInstance().saveAggregateData(aggregateData);
+    await MongoOperator.getInstance().saveHistoryData(historyData);
+  }
 
   serverStartUp() {
     Scheduler.getInstance().register(
