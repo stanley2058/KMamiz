8cd793c5e3dc4d150a5bfd2bc4d1b4687bf949df
stanley2058 <stanley2058@yahoo.com.tw>
2022-02-21 14:19:07 +0800

    feat: add swagger endpoint

---
+++ src/handler/SwaggerService.ts
@@ -0,0 +1,32 @@
+import IRequestHandler from "../entities/IRequestHandler";
+import swaggerUi from "swagger-ui-express";
+import SwaggerUtils from "../utils/SwaggerUtils";
+import MongoOperator from "../services/MongoOperator";
+
+export default class SwaggerService extends IRequestHandler {
+  constructor() {
+    super("swagger");
+    this.addRoute("get", "/:uniqueServiceName", async (req, res) => {
+      const uniqueServiceName = req.params?.uniqueServiceName;
+      if (!uniqueServiceName) res.sendStatus(400);
+      res
+        .type("html")
+        .send(await this.getSwagger(decodeURIComponent(uniqueServiceName)));
+    });
+  }
+
+  async getSwagger(uniqueServiceName: string) {
+    const [service, namespace, version] = uniqueServiceName.split("\t");
+    const endpoints =
+      await MongoOperator.getInstance().getEndpointDataTypeByService(
+        uniqueServiceName
+      );
+    return swaggerUi.generateHTML(
+      SwaggerUtils.FromEndpoints(
+        `${service}.${namespace}`,
+        version,
+        endpoints.map((e) => e.endpointDataType)
+      )
+    );
+  }
+}
--- src/handler/index.ts
+++ src/handler/index.ts
@@ -1,2 +1,3 @@
 export { default as DataHandler } from "./DataService";
 export { default as GraphHandler } from "./GraphService";
+export { default as SwaggerService } from "./SwaggerService";
