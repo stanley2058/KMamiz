b4d69010c350f81286e11c039a0eed8b378bc4ba
stanley2058 <stanley2058@yahoo.com.tw>
2023-03-28 18:39:36 +0800

    feat: impl JSON object merge & ts type generate

--- kmamiz_data_processor/src/data/realtime_data.rs
+++ kmamiz_data_processor/src/data/realtime_data.rs
@@ -1,10 +1,13 @@
 use std::collections::HashMap;
 
+use crate::json_utils;
+
 use super::{
-    combined_realtime_data::{CombinedLatency, PartialCombinedRealtimeData},
+    combined_realtime_data::{CombinedLatency, CombinedRealtimeData, PartialCombinedRealtimeData},
     request_type::RequestType,
 };
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
@@ -26,6 +29,10 @@ pub struct RealtimeData {
 }
 
 impl RealtimeData {
+    pub fn combine(data: Vec<RealtimeData>) -> Vec<CombinedRealtimeData> {
+        Self::complete_partial(Self::partial_combine(data))
+    }
+
     pub fn partial_combine(data: Vec<RealtimeData>) -> Vec<PartialCombinedRealtimeData> {
         let mut name_mapping = HashMap::new();
         data.into_iter().for_each(|d| {
@@ -93,6 +100,47 @@ impl RealtimeData {
                     request_body,
                     response_body,
                     avg_replica: total_replicas as f64 / combined,
+                }
+            })
+            .collect()
+    }
+
+    pub fn complete_partial(data: Vec<PartialCombinedRealtimeData>) -> Vec<CombinedRealtimeData> {
+        data.into_iter()
+            .map(|d| {
+                let request_body = d
+                    .request_body
+                    .into_iter()
+                    .filter_map(|req| serde_json::from_str(&req).ok())
+                    .collect::<Vec<Value>>();
+                let request_body = json_utils::merge(request_body);
+
+                let response_body = d
+                    .response_body
+                    .into_iter()
+                    .filter_map(|res| serde_json::from_str(&res).ok())
+                    .collect::<Vec<Value>>();
+                let response_body = json_utils::merge(response_body);
+
+                CombinedRealtimeData {
+                    _id: None,
+                    unique_service_name: d.unique_service_name,
+                    unique_endpoint_name: d.unique_endpoint_name,
+                    latest_timestamp: d.latest_timestamp,
+                    method: d.method,
+                    service: d.service,
+                    namespace: d.namespace,
+                    version: d.version,
+                    latency: d.latency,
+                    combined: d.combined,
+                    status: d.status,
+                    request_body: serde_json::to_string(&request_body).ok(),
+                    request_schema: Some(json_utils::to_types(request_body)),
+                    request_content_type: d.request_content_type,
+                    response_body: serde_json::to_string(&response_body).ok(),
+                    response_schema: Some(json_utils::to_types(response_body)),
+                    response_content_type: d.response_content_type,
+                    avg_replica: d.avg_replica,
                 }
             })
             .collect()
